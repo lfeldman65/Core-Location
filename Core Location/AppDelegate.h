1ef6d8692f9c333779100affbb4cef40ccd45020
Larry Feldman <larryfeldman@Larrys-MacBook-Pro.local>
2015-05-22 12:23:41 -0500

    Initial Commit

---
+++ Core Location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core Location
+//
+//  Created by Larry Feldman on 5/22/15.
+//  Copyright (c) 2015 Larry Feldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Core Location/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core Location
+//
+//  Created by Larry Feldman on 5/22/15.
+//  Copyright (c) 2015 Larry Feldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
