68f6318db74de23cccda2d40da927a50066324d7
Larry Feldman <larryfeldman@Larrys-MacBook-Pro.local>
2015-05-22 13:12:28 -0500

    Latitude, Longitude and Address show reasonable results in simulator.

--- Core Location/ViewController.m
+++ Core Location/ViewController.m
@@ -9,19 +9,83 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *latitude;
+@property (weak, nonatomic) IBOutlet UILabel *longitude;
+@property (weak, nonatomic) IBOutlet UILabel *address;
+- (IBAction)buttonPressed:(id)sender;
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    
+    CLLocationManager *manager;
+    CLGeocoder *geocoder;
+    CLPlacemark *placemark;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    manager = [[CLLocationManager alloc] init];
+    geocoder = [[CLGeocoder alloc] init];
+    [manager requestAlwaysAuthorization];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)buttonPressed:(id)sender {
+    
+    manager.delegate = self;
+    manager.desiredAccuracy = kCLLocationAccuracyBest;
+    [manager startUpdatingLocation];
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    
+    NSLog(@"Error: %@", error);
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    
+    
+    if ([locations lastObject] != nil) {
+        
+        CLLocation *lastLocation = [locations lastObject];
+
+        self.latitude.text = [NSString stringWithFormat:@"%.8f", lastLocation.coordinate.latitude];
+        self.longitude.text = [NSString stringWithFormat:@"%.8f", lastLocation.coordinate.longitude];
+
+    //    NSLog(@"%@", [locations lastObject]);
+        
+        [geocoder reverseGeocodeLocation:lastLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+            
+            if (error == nil && [placemarks count] > 0) {
+                
+                placemark = [placemarks lastObject];
+                
+                self.address.text = [NSString stringWithFormat:@"%@ %@\n%@, %@ %@\n%@",
+                                     placemark.subThoroughfare, placemark.thoroughfare,
+                                     placemark.locality, placemark.administrativeArea, placemark.postalCode,                                                                    
+                                     placemark.country];
+            } else {
+                
+                NSLog(@"%@", error.debugDescription);
+            }
+            
+        }];
+        
+    }
+}
+
+
+
+
+
 
 @end
